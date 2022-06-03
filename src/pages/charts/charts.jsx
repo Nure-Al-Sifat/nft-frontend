4794fcb20e97b58f8dc5be2c712bb253341944f3
Nure Al Sifat <75719853+Nure-Al-Sifat@users.noreply.github.com>
2022-06-03 18:21:14 +0600

    new update

---
+++ src/pages/charts/charts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+function charts() {
+  return (
+    <>
+        <Sidebar/>
+    </>
+  )
+}
+
+export default charts
